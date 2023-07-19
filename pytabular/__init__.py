86fc2634960b1adf73882003c54d042e8289f585
Gage Renzi <gage.renzi@healthstream.com>
2023-07-19 14:44:32 -0500

    remove some logging junk

--- pytabular/__init__.py
+++ pytabular/__init__.py
@@ -7,35 +7,36 @@ Then it will begin to import specifics of the module.
 # flake8: noqa
 import logging
 import os
-import sys
 import platform
-from rich.logging import RichHandler
-from rich.theme import Theme
-from rich.console import Console
-from rich import pretty
+import sys
+
+# from rich import pretty
+# from rich.console import Console
+# from rich.logging import RichHandler
+# from rich.theme import Theme
 
 pretty.install()
-console = Console(theme=Theme({"logging.level.warning": "bold reverse red"}))
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(message)s",
-    datefmt="[%H:%M:%S]",
-    handlers=[RichHandler(console=console)],
-)
+# console = Console(theme=Theme({"logging.level.warning": "bold reverse red"}))
+# logging.basicConfig(
+#     level=logging.DEBUG,
+#     format="%(message)s",
+#     datefmt="[%H:%M:%S]",
+#     # handlers=[RichHandler(console=console)],
+# )
 logger = logging.getLogger("PyTabular")
-logger.setLevel(logging.INFO)
-logger.info("Logging configured...")
-logger.info("To update logging:")
-logger.info(">>> import logging")
-logger.info(">>> pytabular.logger.setLevel(level=logging.INFO)")
-logger.info("See https://docs.python.org/3/library/logging.html#logging-levels")
+# logger.setLevel(logging.INFO)
+# logger.info("Logging configured...")
+# logger.info("To update logging:")
+# logger.info(">>> import logging")
+# logger.info(">>> pytabular.logger.setLevel(level=logging.INFO)")
+# logger.info("See https://docs.python.org/3/library/logging.html#logging-levels")
 
 
-logger.info(f"Python Version::{sys.version}")
-logger.info(f"Python Location::{sys.exec_prefix}")
-logger.info(f"Package Location:: {__file__}")
-logger.info(f"Working Directory:: {os.getcwd()}")
-logger.info(f"Platform:: {sys.platform}-{platform.release()}")
+# logger.info(f"Python Version::{sys.version}")
+# logger.info(f"Python Location::{sys.exec_prefix}")
+# logger.info(f"Package Location:: {__file__}")
+# logger.info(f"Working Directory:: {os.getcwd()}")
+# logger.info(f"Platform:: {sys.platform}-{platform.release()}")
 
 dll = os.path.join(os.path.dirname(__file__), "dll")
 sys.path.append(dll)
@@ -52,18 +53,14 @@ logger.info("Adding Reference Microsoft.AnalysisServices")
 clr.AddReference("Microsoft.AnalysisServices")
 
 logger.info("Importing specifics in module...")
-from .pytabular import Tabular
-
-from .logic_utils import (
-    pd_dataframe_to_m_expression,
-    pandas_datatype_to_tabular_datatype,
-)
-from .tabular_tracing import BaseTrace, RefreshTrace, QueryMonitor
-from .tabular_editor import TabularEditor
 from .best_practice_analyzer import BPA
-from .query import Connection
-from .pbi_helper import find_local_pbi_instances
 from .document import ModelDocumenter
-
+from .logic_utils import (pandas_datatype_to_tabular_datatype,
+                          pd_dataframe_to_m_expression)
+from .pbi_helper import find_local_pbi_instances
+from .pytabular import Tabular
+from .query import Connection
+from .tabular_editor import TabularEditor
+from .tabular_tracing import BaseTrace, QueryMonitor, RefreshTrace
 
 logger.info("Import successful...")
